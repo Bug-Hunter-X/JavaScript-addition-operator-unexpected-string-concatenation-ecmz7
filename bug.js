5d484979494d3ca497d37cf8968053c4d9a1edde
BugHunter-X <askzenai@gmail.com>
2025-01-23 20:20:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(1, '2')); // Output: "12"
+
+//In JavaScript, the + operator will perform string concatenation if one of the operands is a string.  This can lead to unexpected results when working with numbers.
